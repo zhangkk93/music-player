54de516475b006228b4ea639c353a797d9b2d542
zhangkk93 <zkk_93@163.com>
2018-03-28 16:06:30 +0800

    create

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    Hello world
+  </div>
+</template>
+<script>
+
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "common/stylus/variable"
+#app 
+  color:$color-theme
+</style>
